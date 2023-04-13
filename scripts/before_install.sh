ac351f5978787e9420418e1a312b6bc8716b4b9c
EC2 Default User <ec2-user@ip-172-31-2-19.eu-west-1.compute.internal>
2023-04-13 20:45:38 +0000

    some more changes

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -8,7 +8,7 @@ sudo yum -y update
 sudo yum -y install ruby
 sudo yum -y install wget
 cd /home/ec2-user
-wget https://aws-codedeploy-us-west-1.s3.amazonaws.com/latest/install
+wget https://aws-codedeploy-eu-west-1.s3.amazonaws.com/latest/install
 sudo chmod +x ./install
 sudo ./install auto
 
