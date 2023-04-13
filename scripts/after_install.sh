b66babb7276f611096bcae7d217796d748e90f5e
priyankajoseph8 <ec2-user@ip-172-31-29-196.eu-west-1.compute.internal>
2023-04-13 16:38:48 +0000

    codedeploy changes

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -7,14 +7,14 @@ sudo pkill -f runserver
 # sudo pkill -f tailwind
 # sudo pkill -f node
 
-cd /home/ubuntu/django-aws_cicd/
+cd /home/ec2-user/django-aws_cicd/
 
 # activate virtual environment
 python3 -m venv venv
 source venv/bin/activate
 
 install requirements.txt
-pip install -r /home/ubuntu/django-aws_cicd/requirements.txt
+pip install -r /home/ec2-user/django-aws_cicd/requirements.txt
 
 # run server
 screen -d -m python3 manage.py runserver 0:800
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # clean codedeploy-agent files for a fresh install
-sudo rm -rf /home/ubuntu/install
+sudo rm -rf /home/ec2-user/install
 
 # install CodeDeploy agent
 sudo apt-get -y update
 sudo apt-get -y install ruby
 sudo apt-get -y install wget
-cd /home/ubuntu
+cd /home/ec2-user
 wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
 sudo chmod +x ./install 
 sudo ./install auto
